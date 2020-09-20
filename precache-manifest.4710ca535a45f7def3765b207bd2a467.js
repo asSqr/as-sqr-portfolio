8026943363e333608d28b726a473ff88bcf1bf5e
Ysmr-Ry <ryo.yossy@live.jp>
2020-09-21 06:53:18 +0900

    Updates

---
+++ precache-manifest.4710ca535a45f7def3765b207bd2a467.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a10ac9bb002714c89aa681fce4c59a1a",
+    "url": "/as-sqr-portfolio/index.html"
+  },
+  {
+    "revision": "a8824c8049fbcb89eee6",
+    "url": "/as-sqr-portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b79443e3c924e5190494",
+    "url": "/as-sqr-portfolio/static/js/2.65489920.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/as-sqr-portfolio/static/js/2.65489920.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8824c8049fbcb89eee6",
+    "url": "/as-sqr-portfolio/static/js/main.749442c2.chunk.js"
+  },
+  {
+    "revision": "17a6c59761d92f27ca64",
+    "url": "/as-sqr-portfolio/static/js/runtime-main.25ad6964.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/as-sqr-portfolio/precache-manifest.4710ca535a45f7def3765b207bd2a467.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/as-sqr-portfolio/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
