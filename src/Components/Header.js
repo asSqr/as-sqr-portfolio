53d84aa39dc71158bfbda3c01cabe0ab7389e53a
Ysmr-Ry <ryo.yossy@live.jp>
2020-09-21 09:17:02 +0900

    resumeData.json moved

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -20,7 +20,6 @@ class Header extends Component {
             <li><a className="smoothscroll" href="#resume">Resume</a></li>
             <li><a className="smoothscroll" href="#portfolio">Works</a></li>
             <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
-            <li><a className="smoothscroll" href="#contact">Contact</a></li>
           </ul>
         </nav>
 
