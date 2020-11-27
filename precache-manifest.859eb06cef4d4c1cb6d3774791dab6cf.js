8e73f2e90a1a03ee18ec06723249e480a10f100c
asSqr <ryo.yossy@live.jp>
2020-11-27 17:02:52 +0900

    Updates

--- precache-manifest.859eb06cef4d4c1cb6d3774791dab6cf.js
+++ precache-manifest.859eb06cef4d4c1cb6d3774791dab6cf.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cc7359e28ac0cf87c0b1bf020b406dc4",
+    "revision": "4194bf0296b74d7ceaf20bbf58553edc",
     "url": "/as-sqr-portfolio/index.html"
   },
   {
-    "revision": "750273fc2aa258bee72f",
-    "url": "/as-sqr-portfolio/static/css/main.5f361e03.chunk.css"
+    "revision": "47407e57452eff2a2ed3",
+    "url": "/as-sqr-portfolio/static/css/main.523f09c5.chunk.css"
   },
   {
     "revision": "ea55a8717abc59969fce",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/as-sqr-portfolio/static/js/2.2c4b950b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "750273fc2aa258bee72f",
-    "url": "/as-sqr-portfolio/static/js/main.4c178e3a.chunk.js"
+    "revision": "47407e57452eff2a2ed3",
+    "url": "/as-sqr-portfolio/static/js/main.b5fb1dec.chunk.js"
   },
   {
     "revision": "17a6c59761d92f27ca64",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/as-sqr-portfolio/precache-manifest.cd951eb813425837c8073f5419cb7eaa.js"
+  "/as-sqr-portfolio/precache-manifest.859eb06cef4d4c1cb6d3774791dab6cf.js"
 );
 
 self.addEventListener('message', (event) => {
