c98b4cbdf6c8aff1f1cc5cee569f24b17f34de6d
Ysmr-Ry <ryo.yossy@live.jp>
2021-10-15 23:04:27 +0900

    fix: blog added

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -138,7 +138,7 @@ class Portfolio extends Component {
             </div>
           </div>
         </div>
-        {/*
+        {
         <div className="row">
           <div className="twelve columns collapsed">
             <h1>かいたもの</h1>
@@ -146,7 +146,7 @@ class Portfolio extends Component {
             {blogs}
           </div>
         </div>
-        */}
+        }
 
         {
           this.state.open && (
